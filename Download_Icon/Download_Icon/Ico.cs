ab219ba585b6ba19dee40ef8155bf43abe1ca326
Eduardo <eduardo.martinez.catala@gmail.com>
2013-04-23 23:10:30 +0200

    Planteamiento

---
+++ Download_Icon/Download_Icon/Ico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+namespace Download_Icon
+{
+    class Ico
+    {
+        public BitmapImage imagen { get; set; }
+        private String ruta { get; set; }
+        public String host { get; set; }
+        public Ico(String ruta)
+        {
+            this.ruta = ruta;
+            Dowload_ico();
+        }
+        private void Dowload_ico()
+        {
+            Uri a = new Uri(ruta, UriKind.RelativeOrAbsolute);
+            host = a.Host;
+            System.Windows.Media.Imaging.BitmapImage temp = new System.Windows.Media.Imaging.BitmapImage();
+            ruta = ruta + "/favicon.ico";
+            temp.BeginInit();
+            temp.UriSource = new Uri(ruta, UriKind.RelativeOrAbsolute);
+            temp.EndInit();
+            imagen = temp;
+        }
+    }
+}
---
+++ Download_Icon/Download_Icon/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Download_Icon
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Ico temp;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            temp = new Ico(url.Text);
+            icono.Source = temp.imagen;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+               Microsoft.Win32.SaveFileDialog dialogo1 = new Microsoft.Win32.SaveFileDialog();
+                    // dialogo1.ShowDialog();
+                    dialogo1.AddExtension = false;
+                    dialogo1.FileName = temp.host + ".ico";
+
+                    if (dialogo1.ShowDialog() == true)
+                    {
+                        FileStream stream5 = new FileStream(dialogo1.FileName, FileMode.Create);
+                        TiffBitmapEncoder encoder5 = new TiffBitmapEncoder();
+                        encoder5.Frames.Add(BitmapFrame.Create(temp.imagen));
+                        encoder5.Save(stream5);
+                    }
+        }
+
+    }
+}
